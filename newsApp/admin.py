a4a6684ef7d4eb4781285244d694fe36ad44132b
Enxhi Gjata <102411095+enxhigjata@users.noreply.github.com>
2022-09-15 15:25:29 +0200

    001

---
+++ newsApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from newsApp import models
+# Register your models here.
+
+admin.site.register(models.Category)
+admin.site.register(models.Post)
+admin.site.register(models.Comment)
+admin.site.register(models.Kryesoret)
